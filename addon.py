d57de88c835939d51d741e0eae6d1ecb7856cbaf
pugilist <myevilempire@gmail.com>
2016-09-29 16:32:24 -0400

    reverted a change to restore functionality to _fetch_nested_data()

--- addon.py
+++ addon.py
@@ -231,11 +231,11 @@ class BassDrive:
         :param key_names: a list of strings that represent different layers of the datastructure
         :param data_structure: the structure containing all directories and filenames available.
         """
-        key = key_names[0]
+        key = key_names.pop()
 
         # If our first key is "Archives", then just get the next one.
         if key == "Archives":
-            key = key_names[0]
+            key = key_names.pop()
 
         for item in data_structure:
             if isinstance(item, dict) and item.keys()[-1] == key:
